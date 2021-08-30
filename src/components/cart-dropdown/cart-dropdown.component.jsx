64c7fedf426de958b1fbd8031697388e50662201
Fabricio Reche <fabricio.reche@dextra-sw.com>
2021-08-30 14:53:05 -0300

    add styled-components to some components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,25 +4,26 @@ import { withRouter } from 'react-router';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { connect } from 'react-redux';
-import './cart-dropdown.styles.scss';
+
+import { CartDropdownContainer, CartDropdownEmptyMessage, CartDropdownItemsContainer } from './cart-dropdown.styles';
 
 const CartDropdown = ({cartItems, history, dispatch})=> (
-    <div className="cart-dropdown">
-        <div className="cart-items">
+    <CartDropdownContainer>
+        <CartDropdownItemsContainer>
             {
                 cartItems.length ?
                 cartItems.map( item => (
                     <CartItem key={item.id} item={item}/>
                 ))
                 :
-                (<span className="empty-message">Your cart is empty.</span>)
+                (<CartDropdownEmptyMessage>Your cart is empty.</CartDropdownEmptyMessage>)
             }
-        </div>
+        </CartDropdownItemsContainer>
         <CustomButtom onClick={()=>{
             history.push('/checkout');
             dispatch(toggleCartHidden());
         }}>GO TO CHECKOUT</CustomButtom>
-    </div>
+    </CartDropdownContainer>
 );
 
 const mapStateToProps = state => ({
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -9,24 +8,26 @@ import { connect } from 'react-redux';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
+import { HeaderContainer, LogoContainer, OptionLink, OptionsContainer } from './header.styles';
+
 const Header = ({ currentUser, hidden }) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
-            <Logo className="logo" />
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">SHOP</Link>
-            <Link className="option" to="/shop">CONTACT</Link>
+    <HeaderContainer>
+        <LogoContainer to="/">
+            <Logo />
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to="/shop">SHOP</OptionLink>
+            <OptionLink to="/shop">CONTACT</OptionLink>
             {
                 currentUser ?
-                <div className="option" onClick={()=> auth.signOut()}> SIGN OUT</div>
+                <OptionLink as='div' onClick={()=> auth.signOut()}> SIGN OUT</OptionLink>
                 :
-                <Link className="option" to="/signin">SIGN IN</Link>
+                <OptionLink to="/signin">SIGN IN</OptionLink>
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {hidden ? null : <CartDropdown />}
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
